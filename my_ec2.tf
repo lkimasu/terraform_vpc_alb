3cb5a6de0e51d5576dd09bc5281c1769d6e72fd1
System Administrator <root@gts-MacBook-Air.local>
2023-01-04 15:18:19 +0900

    add bastion host

--- my_ec2.tf
+++ my_ec2.tf
@@ -3,7 +3,7 @@ resource "aws_key_pair" "web_admin" {
   public_key = file("~/.ssh/web_admin.pub")
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "bastion" {
  ami = "ami-035233c9da2fabf52"
  instance_type = "t2.micro"
  key_name = aws_key_pair.web_admin.key_name
@@ -15,7 +15,7 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "my_security" {
   name = "allow_ssh_from_all"
-  description = "Allow SSH,HTTP allow"
+  description = "Allow SSH allow"
   vpc_id = aws_vpc.my_vpc.id
 
   ingress {
@@ -24,12 +24,35 @@ resource "aws_security_group" "my_security" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+resource "aws_instance" "web" {
+ ami = "ami-035233c9da2fabf52"
+ instance_type = "t2.micro"
+ key_name = aws_key_pair.web_admin.key_name
+ subnet_id = aws_subnet.Private-Subnet-A.id
+ vpc_security_group_ids = [
+ aws_security_group.my_security1.id
+ ]
+}
+
+resource "aws_security_group" "my_security1" {
+  name = "bastion host to private EC2"
+  description = "bastion host to private EC2"
+  vpc_id = aws_vpc.my_vpc.id
 
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.my_security.id]
   }
   egress {
     from_port = 0
