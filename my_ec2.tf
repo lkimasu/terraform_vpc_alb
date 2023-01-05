4046b4b6641d87aa72704adb4f2a01506c2e385d
lkimasu <lwlstjdl@naver.com>
2023-01-05 14:21:53 +0900

    add private ec2

--- my_ec2.tf
+++ my_ec2.tf
@@ -41,6 +41,34 @@ resource "aws_instance" "web" {
  vpc_security_group_ids = [
  aws_security_group.my_security1.id
  ]
+ user_data = <<EOF
+            #! /bin/bash
+            sudo yum update -y
+            sudo yum install -y httpd
+            sudo systemctl start httpd
+            sudo systemctl enable httpd
+            echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+            EOF
+}
+
+resource "aws_instance" "web_sub" {
+ ami = "ami-035233c9da2fabf52"
+ instance_type = "t2.micro"
+ key_name = aws_key_pair.web_admin.key_name
+ subnet_id = aws_subnet.Private-Subnet-B.id
+ vpc_security_group_ids = [
+ aws_security_group.my_security1.id
+ ]
+ user_data = <<EOF
+            #!/bin/bash
+            # Use this for your user data (script from top to bottom)
+            # install httpd (Linux 2 version)
+            sudo yum update -y
+            sudo yum install -y httpd
+            sudo systemctl start httpd
+            sudo systemctl enable httpd
+            echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+            EOF
 }
 
 resource "aws_security_group" "my_security1" {
@@ -54,10 +82,16 @@ resource "aws_security_group" "my_security1" {
     protocol = "tcp"
     security_groups = [aws_security_group.my_security.id]
   }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_groups = [aws_security_group.my_alb_security.id]
+  }
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
- }
+  }
 }
